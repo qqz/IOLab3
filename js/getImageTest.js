a5651fc6bc2c8e4d021d0f775da1f308cb80bd07
Michael Hintze <hintze.michael@gmail.com>
2012-10-31 02:23:35 -0700

    commented out errors.

--- js/getImageTest.js
+++ js/getImageTest.js
@@ -1,13 +1,18 @@
 
 $(document).ready(function(){
 	
-	// Do something every 5 seconds
-	setInterval(function() {
+	//Do something every 5 seconds
+	// setInterval(function() {
+	// 	getInstagram();
+	// }, 1000);
+
+	//Do something on click
+	$("#test_link_button").click(function() {
 		getInstagram();
-	}, 1000);
+	});
 
 	function getInstagram(){
-		var str = 'https://api.instagram.com/v1/media/search?lat=37.786403&lng=-122.405033&distance=1800&min_timestamp=1351656000';
+		var str = 'https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.2943&distance=12000&min_timestamp=1351429688';
 
 		// guys, this is my super secret Instagram client ID
 		str = str + '&client_id=ecbef9870b1f4200a72dd0c95fa66941';
@@ -18,29 +23,30 @@ $(document).ready(function(){
 				var objects = []; // an array of objects
 
 				$.each(json, function(index, item){
-
 					$.each(item, function(index, v){
-						console.log(v);
 						var image = {}; // a new object.
 						image.id = v.id; // and its information
 						image.link = v.link;
 						image.created_time = v.created_time;
+						image.tags = v.tags;
+						image.images = v.images;
 
-						$('<p>'+v.id+','+v.link+','+v.created_time+'</p>').appendTo('#results');
+						$('<p>'+v.id+','+v.link+','+v.created_time+','+v.tags+'</p>').appendTo('#results');
+						
 						objects.push(image); // push image onto objects array
 			    	});
 			    });
 
-					// Finally, send your array of objects to the PHP file
-					 $.ajax({
-						 url: "http://chancezhao.com/insert.php",
-						 type: "POST",
-						 data: { objects: objects },
-						 cache: false,
-						 success: function () {
-							 console.log("SUCCESS WOOOOO");
-						 }
-					 });
+					// // Finally, send your array of objects to the PHP file
+					//  $.ajax({
+					// 	 url: "http://chancezhao.com/insert.php",
+					// 	 type: "POST",
+					// 	 data: { objects: objects },
+					// 	 cache: false,
+					// 	 success: function () {
+					// 		 console.log("SUCCESS WOOOOO");
+					// 	 }
+					//  });
 				
 
 				
