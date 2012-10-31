d544de35e310b25b99a0805210395316b0cb529a
Qianqian Zhao <chance.zhao@gmail.com>
2012-10-31 00:52:53 -0800

    edits to getImageTest

--- js/getImageTest.js
+++ js/getImageTest.js
@@ -14,13 +14,35 @@ $(document).ready(function(){
 
 		$.getJSON(str+'&callback=?', 
 			function(json){ 
+
+				var objects = []; // an array of objects
+
 				$.each(json, function(index, item){
 
 					$.each(item, function(index, v){
 						console.log(v);
+						var image = {}; // a new object.
+						image.id = v.id; // and its information
+						image.link = v.link;
+						image.created_time = v.created_time;
+
 						$('<p>'+v.id+','+v.link+','+v.created_time+'</p>').appendTo('#results');
+						objects.push(image); // push image onto objects array
 			    	});
 			    });
+
+					// Finally, send your array of objects to the PHP file
+					 $.ajax({
+						 url: "http://chancezhao.com/insert.php",
+						 type: "POST",
+						 data: { objects: objects },
+						 cache: false,
+						 success: function () {
+							 console.log("SUCCESS WOOOOO");
+						 }
+					 });
+				
+
 				
 			}); // end of getJSON
 		
