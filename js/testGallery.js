f41a95593bf0ce8f71bd2e765387a2a35977f7ba
Arthur Che <che@ischool.berkeley.edu>
2012-11-13 13:58:56 -0800

    added convertTime function

--- js/testGallery.js
+++ js/testGallery.js
@@ -1,5 +1,19 @@
 var selected_time = 1351699200;
 
+function convertTime(unixtime) {
+    var date = new Date((unixtime-3600)*1000);
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = "AM";
+    if (hours > 11) { ampm = "PM" }
+    if (hours > 12) { hours = hours - 12; }
+    if (hours == 0) { hours = 12; }
+    if (minutes < 10) { minutes = "0" + minutes }
+    if (minutes == 0) { minutes = "00" } 
+    var currentTime = hours+":"+minutes+ampm;
+    return currentTime
+};
+
 $(function() {
 
     $("#slider").slider({
@@ -10,42 +24,20 @@ $(function() {
 
         // set initial time
         create: function(event,ui) {
+            // update selected_time to new slider value
             selected_time = $(this).slider('value');
-            // convert time from Unix to regular AM/PM
-            var date = new Date((selected_time-3600)*1000);
-            var hours = date.getHours();
-            var minutes = date.getMinutes();
-            var ampm = "AM";
-            if (hours > 11) { ampm = "PM" }
-            if (hours > 12) { hours = hours - 12; }
-            if (hours == 0) { hours = 12; }
-            if (minutes < 10) { minutes = "0" + minutes }
-            if (minutes == 0) { minutes = "00" } 
-            var currentTime = hours+":"+minutes+ampm;
-
             // update #time div
-            $('#time').html('Time: '+currentTime);
+            $('#time').html('Time: '+convertTime(selected_time));
         },
 
         // set time on slide
     	slide: function(event,ui) {
+            // update selected_time to new slider value
             selected_time = ui.value;
             console.log(selected_time);
-            // convert time from Unix to regular AM/PM
-            var date = new Date((selected_time-3600)*1000);
-            var hours = date.getHours();
-            var minutes = date.getMinutes();
-            var ampm = "AM";
-            if (hours > 11) { ampm = "PM" }
-            if (hours > 12) { hours = hours - 12; }
-            if (hours == 0) { hours = 12; }
-            if (minutes < 10) { minutes = "0" + minutes }
-            if (minutes == 0) { minutes = "00" } 
-            var currentTime = hours+":"+minutes+ampm;
-            console.log(currentTime);
-
             // update #time div
-            $('#time').html('Time: '+currentTime);
+            $('#time').html('Time: '+convertTime(selected_time));
+            console.log(convertTime(selected_time))
     	}
 
     });
