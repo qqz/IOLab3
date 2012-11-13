f7b3bfd04b765519f56d2e4307fbb962bb4ab094
Arthur Che <che@ischool.berkeley.edu>
2012-11-12 16:43:17 -0800

    added 30min increment

--- js/testGallery.js
+++ js/testGallery.js
@@ -1,34 +1,18 @@
 $(function() {
-    var slider = $('#slider'); 
-    var time = $('#time');
 
-    slider.slider({
+    $("#slider").slider({
     	min: 1351699200,
     	max: 1351728000,
     	value: 1351699200,
-
-    	start: function(event,ui) {
-    		var value = slider.slider('value');
-    		time.text('time: '+value);
-            // $("#time").fadeIn(200);
-    	},
+        step: 1800,
 
     	slide: function(event,ui) {
-    		var value = slider.slider('value');
-            // $("#time").fadeIn(200);
-    		time.text('time: '+value);
-    	},
-
-        change: function(event,ui) {
-            // $("#time").fadeOut(8000);
-        },
-
+            $('#time').text('time: '+ui.value);
+            
+    	}
     });
-});
 
-// $("#tag-button").click(function() {
-//   $(".tag-box").toggle();
-// })
+});
 
 $("#tag-button").toggle(
   function(){
